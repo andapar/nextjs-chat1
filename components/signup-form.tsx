55d75c681381d68abc6dac57eb8db4906fccab63
andapar <160366488+andapar@users.noreply.github.com>
2024-07-29 01:18:51 +0300

    Update signup-form.tsx

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -5,12 +5,10 @@ import { useState } from 'react';
 import { toast } from 'sonner';
 import Link from 'next/link';
 import { IconSpinner } from './ui/icons';
-import { useRouter } from 'next/navigation';
 
 export default function SignupForm() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
 
   const handleSignIn = async () => {
     setLoading(true);
@@ -21,8 +19,8 @@ export default function SignupForm() {
         setError("Kayıt başarısız.");
         toast.error("Kayıt başarısız.");
       } else {
-        toast.success("Kayıt başarılı! Lütfen e-postanızı kontrol edin ve hesabınızı onaylayın.");
-        router.refresh();
+        toast.success("Kayıt başarılı!");
+        window.location.reload();
       }
     } catch (err) {
       setError("Bir hata oluştu.");
@@ -35,7 +33,7 @@ export default function SignupForm() {
   return (
     <form className="flex flex-col items-center gap-4 space-y-3">
       <div className="w-full flex-1 rounded-lg border bg-white px-6 pb-4 pt-8 shadow-md md:w-96 dark:bg-zinc-950">
-        <h1 className="mb-3 text-2xl font-bold">Sign up to continue.</h1>
+        <h1 className="mb-3 text-2xl font-bold">Devam etmek için lütfen kayıt olun.</h1>
         <button
           type="button"
           className="my-4 flex h-10 w-full flex-row items-center justify-center rounded-md bg-zinc-900 p-2 text-sm font-semibold text-zinc-100 hover:bg-zinc-800 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-200"
@@ -51,7 +49,7 @@ export default function SignupForm() {
         href="/login"
         className="flex flex-row gap-1 text-sm text-zinc-400"
       >
-        Already have an account? <div className="font-semibold underline">Log in</div>
+        Zaten bir hesabınız var mı? <div className="font-semibold underline">Giriş yap</div>
       </Link>
     </form>
   );
