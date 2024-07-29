1facecb14d19d2bd8c9595c986fda73be7f6b858
andapar <160366488+andapar@users.noreply.github.com>
2024-07-29 16:37:16 +0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,7 +1,13 @@
+// auth.ts
+
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
+import { z } from 'zod';
+import { getStringFromBuffer } from './lib/utils';
+import { getUser } from './app/login/actions';
 import { readAllowedEmails, readBlacklist } from './lib/jsonUtils';
+
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -16,40 +22,70 @@ async function fetchJsonData() {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET, // Bu satırı buraya taşıyın
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6)
+          })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+
+          // JSON verilerini API route'dan çekin
+          const { allowedEmails, blacklist } = await fetchJsonData();
+
+          if (!allowedEmails.includes(email)) {
+            throw new Error('Bu e-posta adresi kayıt için izinli değil.');
+          }
+
+          if (blacklist.includes(email)) {
+            throw new Error('Bu kullanıcı oturum açamaz.');
+          }
+
+          const user = await getUser(email);
+
+          if (!user) return null;
+
+          const encoder = new TextEncoder();
+          const saltedPassword = encoder.encode(password + user.salt);
+          const hashedPasswordBuffer = await crypto.subtle.digest(
+            'SHA-256',
+            saltedPassword
+          );
+          const hashedPassword = getStringFromBuffer(hashedPasswordBuffer);
+
+          if (hashedPassword === user.password) {
+            return user;
+          } else {
+            return null;
+          }
+        }
+
+        return null;
+      }
     })
   ],
   callbacks: {
     async session({ session, token }) {
+      // JSON verilerini API route'dan çekin
       const { blacklist } = await fetchJsonData();
 
       if (blacklist.includes(token.email)) {
+        // Kullanıcıyı oturumdan çıkarmak için session.user'ı boş bir nesne ile güncelleyin
         session.user = { id: '', name: '', email: '', image: '' };
       }
       return session;
     },
-    async jwt({ token, user, account }) {
+    async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
         token.email = user.email;
       }
-
-      if (account?.provider === 'google') {
-        const { allowedEmails, blacklist } = await fetchJsonData();
-
-        if (!allowedEmails.includes(token.email)) {
-          throw new Error('Bu e-posta adresi kayıt için izinli değil.');
-        }
-
-        if (blacklist.includes(token.email)) {
-          throw new Error('Bu kullanıcı oturum açamaz.');
-        }
-      }
-
       return token;
     }
   },
